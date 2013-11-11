cde3459fd97995838e3d91cd6279842468ef820a
Marat Kh. Akhin <Marat.Akhin@gmail.com>
2013-11-12 00:15:26 +0400

    Partial implementation of epidemy simulator

--- src/main/scala/simulations/EpidemySimulator.scala
+++ src/main/scala/simulations/EpidemySimulator.scala
@@ -1,6 +1,8 @@
 package simulations
 
-import math.random
+import scala.collection.mutable.{Set => MSet}
+import scala.math.random
+import scala.util.Random
 
 class EpidemySimulator extends Simulator {
 
@@ -11,25 +13,137 @@ class EpidemySimulator extends Simulator {
     val roomRows: Int = 8
     val roomColumns: Int = 8
 
-    // to complete: additional parameters of simulation
+    val modeQuanta = 5
+
+    val hasAirTraffic = true
+    val airTrafficRate = 0.01f
+
+    val prevalenceRate = 0.01f
+    val transmissibilityRate = 0.4f
+    val deathRate = 0.25f
   }
 
   import SimConfig._
 
-  val persons: List[Person] = List() // to complete: construct list of persons
+  // Neighbour lookup map
+  val map = Array.fill(roomRows, roomColumns)(MSet.empty[Person])
 
-  class Person (val id: Int) {
+  // Spawn people
+  val persons: List[Person] = Range(0, population).map(new Person(_)).toList
+  // Infect people
+  persons.take(math.round(prevalenceRate * population)).foreach(_.infected = true)
+  // Kick-start the simulation in random order
+  Random.shuffle(persons).foreach(_.mode())
+
+  def wrapBy(by: Int)(v: Int) = {
+    if (v < 0) by - 1
+    else if (v > by - 1) 0
+    else v
+  }
+
+  def wrapRows(v: Int) = wrapBy(roomRows)(v)
+  def wrapColumns(v: Int) = wrapBy(roomColumns)(v)
+
+  def randomRow() = randomBelow(roomRows)
+  def randomColumn() = randomBelow(roomColumns)
+
+  class Person(val id: Int) {
+
+    // Status
+
     var infected = false
     var sick = false
     var immune = false
     var dead = false
 
-    // demonstrates random number generation
-    var row: Int = randomBelow(roomRows)
-    var col: Int = randomBelow(roomColumns)
+    def isVisiblyInfectious = sick || dead
+    def isInfectious = infected
+    def isHealthy = !infected
 
-    //
-    // to complete with simulation logic
-    //
+    // Actions
+
+    def becomeInfected() {
+      if (isHealthy) {
+        if (map(row)(col).exists(_.isInfectious)) {
+          if (random < transmissibilityRate) {
+            infected = true
+            afterDelay(6) { becomeSick() }
+          }
+        }
+      }
+    }
+
+    def becomeSick() {
+      sick = true
+      afterDelay(8) { becomeDead() }
+    }
+
+    def becomeDead() {
+      if (random < deathRate) {
+        dead = true
+      } else {
+        afterDelay(2) { becomeImmune() }
+      }
+    }
+
+    def becomeImmune() {
+      sick = false
+      immune = true
+      afterDelay(2) { becomeHealthy() }
+    }
+
+    def becomeHealthy() {
+      immune = false
+      infected = false
+    }
+
+    // Movements
+
+    var row: Int = randomRow()
+    var col: Int = randomColumn()
+    map(row)(col) += this
+
+    def updatePos(newRow: Int, newCol: Int) {
+      if (dead) return
+      if (row == newRow && col == newCol) return
+
+      map(row)(col) -= this
+      row = newRow
+      col = newCol
+      map(row)(col) += this
+
+      becomeInfected()
+    }
+
+    def neighbours() = {
+      (row, wrapColumns(col - 1)) ::
+        (row, wrapColumns(col + 1)) ::
+        (wrapRows(row - 1), col) ::
+        (wrapRows(row + 1), col) ::
+        Nil
+    }
+
+    def randomMove() = {
+      if (hasAirTraffic && random < airTrafficRate) {
+        (randomRow(), randomColumn())
+
+      } else {
+        val free = neighbours()
+                   .filterNot(e => map(e._1)(e._2).exists(p => p.isVisiblyInfectious))
+                   .toList
+        if (free.isEmpty) (row, col)
+        else free(randomBelow(free.size))
+
+      }
+    }
+
+    def mode() {
+      val next = randomMove()
+      afterDelay(randomBelow(modeQuanta)) {
+        updatePos(next._1, next._2)
+        mode()
+      }
+    }
   }
+
 }
